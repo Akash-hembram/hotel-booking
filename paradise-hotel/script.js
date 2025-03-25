ed39ef2fbd00c273b2c6bf750a484c1d43b7667d
Akash Hembram <akashhembram.ah@gmail.com>
2025-03-25 16:00:49 +0530

    first commit

---
+++ paradise-hotel/script.js
@@ -0,0 +1,127 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider",{
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  autoplay:{
+    delay:7500,
+    disableOnInteraction:false,
+  },
+  navigation:{
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".room-slider",{
+  spaceBetween:20,
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  autoplay:{
+    delay:7500,
+    disableOnInteraction:false,
+  },
+  pagination:{
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
+var swiper = new Swiper(".gallery-slider",{
+  spaceBetween:10,
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  autoplay:{
+    delay:1500,
+    disableOnInteraction:false,
+  },
+  pagination:{
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+
+var swiper = new Swiper(".review-slider",{
+  spaceBetween:10,
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
+  autoplay:{
+    delay:7500,
+    disableOnInteraction:false,
+  },
+  pagination:{
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+let accordions = document.querySelectorAll('.faqs .row .content .box');
+
+accordions.forEach(acco =>{
+  acco.onclick = () =>{
+    accordions.forEach(subAcco => {subAcco.classList.remove('active')});
+    acco.classList.add('active');
+  }
+})
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("swiper-slide");
+
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+ 
+//   slides[slideIndex-1].style.display = "block";
+
+// }
